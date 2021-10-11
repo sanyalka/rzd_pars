4d50187e248bf4099397b79b3ba09ff342933c91
sanyalka <48321227+sanyalka@users.noreply.github.com>
2021-10-11 19:38:57 +0300

    Create peremenAll.js

---
+++ peremenAll.js
@@ -0,0 +1,10 @@
+var wherePass = document.querySelector("#Page0 > div > div.form-search-bar.j-form-search-bar > div > div.j-route-form-box.route-form-box > form > div > div > div:nth-child(1) > div.rn-ctrl > div > div.input-group.input-group-controllable.input-group-controllable__right > input")
+var whereToPass = document.querySelector("#Page0 > div > div.form-search-bar.j-form-search-bar > div > div.j-route-form-box.route-form-box > form > div > div > div:nth-child(3) > div.rn-ctrl > div > div.input-group.input-group-controllable.input-group-controllable__right > input")
+var datePass = document.querySelector("#Page0 > div > div.form-search-bar.j-form-search-bar > div > div.j-route-form-box.route-form-box > form > div > div > div:nth-child(5) > div.rn-ctrl.rn-required > div > div.input-group.input-group-controllable > div.j-date-inp.date-inp-short.form-control")
+
+let allCart = document.querySelector('.route-cartype-places-left')
+let cvCart = allCart[0].innerHTML //СВ ВАГОН
+let kupeCart = allCart[1].innerHTML //КУПЕ ВАГОН
+let placCart = allCart[2].innerHTML //ПЛАЦКАРТНЫЙ ВАГОН
+
+let numberCart = document.querySelector('route-car-num')
