4e3406e9708338633b97f2a823937bb062100e28
sanyalka <48321227+sanyalka@users.noreply.github.com>
2021-10-11 19:44:53 +0300

    Update peremenAll.js

--- peremenAll.js
+++ peremenAll.js
@@ -3,8 +3,8 @@ var whereToPass = document.querySelector("#Page0 > div > div.form-search-bar.j-f
 var datePass = document.querySelector("#Page0 > div > div.form-search-bar.j-form-search-bar > div > div.j-route-form-box.route-form-box > form > div > div > div:nth-child(5) > div.rn-ctrl.rn-required > div > div.input-group.input-group-controllable > div.j-date-inp.date-inp-short.form-control")
 
 let allCart = document.querySelector('.route-cartype-places-left')
-let cvCart = allCart[0].innerHTML //СВ ВАГОН
-let kupeCart = allCart[1].innerHTML //КУПЕ ВАГОН
-let placCart = allCart[2].innerHTML //ПЛАЦКАРТНЫЙ ВАГОН
+let cvCart = allCart[0] //СВ ВАГОН
+let kupeCart = allCart[1] //КУПЕ ВАГОН
+let placCart = allCart[2] //ПЛАЦКАРТНЫЙ ВАГОН
 
 let numberCart = document.querySelector('route-car-num')
